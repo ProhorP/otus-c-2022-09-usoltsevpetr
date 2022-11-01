40c946ae6955f5a4d3103013b1e2872032a6e28f
Petr Usoltsev <usoltsev05@mail.ru>
2022-11-01 19:59:18 +0800

    HW04 link shared lib + format text by programm indent

--- HW04/main.c
+++ HW04/main.c
@@ -1,30 +1,46 @@
 #include <stdio.h>
-//#include <stdlib.h>
-//#include <sys/stat.h>
-//#include <fcntl.h>
-//#include <unistd.h>
+#include <stdlib.h>
+// #include <sys/stat.h>
+// #include <fcntl.h>
+// #include <unistd.h>
 #include <dlfcn.h>
 
+void error_exit (char *str);
+void *dlopen (const char *filename, int flags);
+
 int
-main(int argc, char *argv[])
+main (int argc, char *argv[])
 {
 
-	if (argc != 2){
-		error_exit("Неверно введена команда\n"
-			"Нужно ввести в таком формате:\n"
-			 "\"./start name_yiur_file\"\n");
-	}
+  if (argc != 2)
+    {
+      error_exit ("Неверно введена команда\n"
+		  "Нужно ввести в таком формате:\n"
+		  "\"./weather city_name\"\n");
+    }
 
+  /* подгрузка библиотеки */
 
-	return 0;
+  void *dyn_lib = dlopen ("libcurl.so", RTLD_LAZY | RTLD_GLOBAL);
+
+/* проверка на успешность */
+
+  if (!dyn_lib)
+    error_exit
+      ("Не удалось подключить динамическую бибилиотеку libcurl.so, не найдена в системe\n");
+
+  /* выгружаем библиотеку */
+  dlclose (dyn_lib);
+
+  return 0;
 
 }
 
 void
-error_exit(char *str){
+error_exit (char *str)
+{
 
-        printf("%s\n", str);
-        exit(1);
+  printf ("%s\n", str);
+  exit (1);
 
 }
-
